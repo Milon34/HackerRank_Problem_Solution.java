edfefbe234a0a01e5409fecff928fd8b1bd097cb
milon34 <milon34cg34@gmail.com>
2021-02-01 12:42:21 +0600

    3

--- Angry_Professor.java
+++ Angry_Professor.java
@@ -1,5 +1,29 @@
 package HackerRank;
 
-public class Angry_Professor {
+import java.util.Scanner;
 
+public class Angry_Professor {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while (t-- >0){
+            int n=sc.nextInt();
+            int k=sc.nextInt();
+            int arr[]=new int[n];
+            for (int i=0;i<n;i++){
+                arr[i]=sc.nextInt();
+            }
+            int count=0;
+            for (int i=0;i<arr.length;i++){
+                if (arr[i]<=0){
+                    count++;
+                }
+            }
+            if (count<k){
+                System.out.println("YES");
+            }else {
+                System.out.println("NO");
+            }
+        }
+    }
 }
